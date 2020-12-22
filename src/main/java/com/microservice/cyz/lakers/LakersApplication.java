0706e76131c92510becedf527ea6ff59fa1fc2a6
yzcui <yzcui@zjft.com>
2020-12-22 10:28:33 +0800

    first commit

---
+++ src/main/java/com/microservice/cyz/lakers/LakersApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.cyz.lakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/cyz/lakers/LakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.cyz.lakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
